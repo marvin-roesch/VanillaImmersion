82cc63b37a0b598c989bf259550f68fe0666f215
Marvin Rösch <marvinroesch99@gmail.com>
2016-09-15 19:06:33 +0200

    Move texture declarations from blockstates to model files
    To improve ease of use for Resource Pack creators, rather than defining textures for most blocks in the blockstates file, they are now directly set inside the models.

--- src/main/kotlin/de/mineformers/vanillaimmersion/client/renderer/EnchantingTableRenderer.kt
+++ src/main/kotlin/de/mineformers/vanillaimmersion/client/renderer/EnchantingTableRenderer.kt
@@ -90,120 +90,121 @@ class EnchantingTableRenderer : TileEntityEnchantmentTableRenderer() {
     override fun renderTileEntityAt(te: TileEntityEnchantmentTable, x: Double, y: Double, z: Double,
                                     partialTicks: Float, destroyStage: Int) {
         // Since we derive from the vanilla renderer, we can't change the type parameters
-        if (te is EnchantingTableLogic) {
-            pushMatrix()
-
-            // Use the lighting of the block directly above the table
-            val light = te.world.getCombinedLight(te.pos.add(0, 1, 0), 0)
-            val bX = light % 65536
-            val bY = light / 65536
-            OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, bX.toFloat(), bY.toFloat())
-
-            // We need to know how much lapis lazuli modifiers the table holds to tell whether its sufficient
-            val modifiers = te[Slot.MODIFIERS]
-            val lapis = modifiers?.stackSize ?: 0
-
-            pushMatrix()
-            // Translate to the table's center
-            translate(x + 0.5, y + 0.75, z + 0.5)
-            enableLighting()
-
-            // What follows is a straight copy from Vanilla, with meaningful names
-            // We don't call the super method because we need these values later on anyway
-            val hover = te.tickCount + partialTicks
-            translate(0.0f, 0.1f + MathHelper.sin(hover * 0.1f) * 0.01f, 0.0f)
-
-            var dYaw = te.bookRotation - te.bookRotationPrev
-            while (dYaw >= Math.PI) {
-                dYaw -= Math.PI.toFloat() * 2f
-            }
-            while (dYaw < -Math.PI) {
-                dYaw += Math.PI.toFloat() * 2f
-            }
-
-            val yaw = te.bookRotationPrev + dYaw * partialTicks
-            rotate(-yaw * (180f / Math.PI.toFloat()), 0.0f, 1.0f, 0.0f)
-            rotate(80.0f, 0.0f, 0.0f, 1.0f)
-
-            var flipLeft = te.pageFlipPrev + (te.pageFlip - te.pageFlipPrev) * partialTicks + 0.25f
-            var flipRight = te.pageFlipPrev + (te.pageFlip - te.pageFlipPrev) * partialTicks + 0.75f
-            flipLeft = (flipLeft - MathHelper.truncateDoubleToInt(flipLeft.toDouble()).toFloat()) * 1.6f - 0.3f
-            flipRight = (flipRight - MathHelper.truncateDoubleToInt(flipRight.toDouble()).toFloat()) * 1.6f - 0.3f
-            if (flipLeft < 0.0f) {
-                flipLeft = 0.0f
-            }
-            if (flipRight < 0.0f) {
-                flipRight = 0.0f
-            }
-            if (flipLeft > 1.0f) {
-                flipLeft = 1.0f
-            }
-            if (flipRight > 1.0f) {
-                flipRight = 1.0f
-            }
-
-            val spread = te.bookSpreadPrev + (te.bookSpread - te.bookSpreadPrev) * partialTicks
-
-            this.bindTexture(BOOK_TEXTURE)
-            enableCull()
-            this.book.render(null, hover, flipLeft, flipRight, spread, 0.0f, 0.0625f)
-            // End of Vanilla part
-
-            popMatrix()
-
-            // We only draw the page content if its actually visible (i.e. book is not closed)
-            if (spread > 0) {
-                renderPages(te, x, y, z, lapis, yaw, hover, flipLeft, spread)
-            }
-
-            // Renders the items
-            color(1f, 1f, 1f, 1f)
-            translate(x + 0.5, y + 1.4, z + 0.5)
-
-            Minecraft.getMinecraft().textureManager.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE)
-            enableRescaleNormal()
-            RenderHelper.enableStandardItemLighting()
-
-            // Seed the animation randomizer with the TE's position, this will make the animations consistent
-            val rand = Random(te.pos.toLong())
-            val bobRand = rand.nextInt(100)
-            // The bobbing should stop momentarily when there is a result
-            val bobTicks = if (te.result == null) te.tickCount + partialTicks else te.bobStop
-            val bob = MathHelper.sin((bobTicks + bobRand) / 10.0f) * 0.1 + 0.1
-            // Only perform the upwards animation if there is a result
-            val t =
-                if (te.result == null)
-                    0.0
-                else
-                    Math.min(te.progress + partialTicks, 40f) / 40.0
-            // Only perform the upwards animation if there is a result
-            val inputAnim =
-                if (te.result != null)
-                    bob + (0.3 - bob) * t
-                else
-                    0.0
-            renderItem(te, Slot.OBJECT, 0.0, bob + inputAnim, 0.0)
-
-            // Get the offsets for each modifier's animation before they are used
-            // Otherwise there might be inconsistency for the positions
-            val lapisOffsets = listOf(rand.nextInt(720), rand.nextInt(720), rand.nextInt(720))
-            val rotationOffset = rand.nextInt(50)
-
-            // Render the modifiers in a circle around the object to enchant
-            for (i in 1..lapis) {
-                val offset = te.tickCount + rotationOffset + partialTicks
-                val bobAngle = Math.toRadians(offset * 4.0 + lapisOffsets[i - 1])
-                val angle = Math.toRadians((360.0 / modifiers!!.stackSize) * i + offset).toFloat()
-                val currentX = MathHelper.sin(angle) * 0.5
-                val currentZ = MathHelper.cos(angle) * 0.5
-                renderItem(te, Slot.MODIFIERS, currentX, Math.sin(bobAngle) * 0.1, currentZ)
-            }
-
-            RenderHelper.disableStandardItemLighting()
-            disableRescaleNormal()
-
-            popMatrix()
+        if (te !is EnchantingTableLogic) {
+            return
         }
+        pushMatrix()
+
+        // Use the lighting of the block directly above the table
+        val light = te.world.getCombinedLight(te.pos.add(0, 1, 0), 0)
+        val bX = light % 65536
+        val bY = light / 65536
+        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, bX.toFloat(), bY.toFloat())
+
+        // We need to know how much lapis lazuli modifiers the table holds to tell whether its sufficient
+        val modifiers = te[Slot.MODIFIERS]
+        val lapis = modifiers?.stackSize ?: 0
+
+        pushMatrix()
+        // Translate to the table's center
+        translate(x + 0.5, y + 0.75, z + 0.5)
+        enableLighting()
+
+        // What follows is a straight copy from Vanilla, with meaningful names
+        // We don't call the super method because we need these values later on anyway
+        val hover = te.tickCount + partialTicks
+        translate(0.0f, 0.1f + MathHelper.sin(hover * 0.1f) * 0.01f, 0.0f)
+
+        var dYaw = te.bookRotation - te.bookRotationPrev
+        while (dYaw >= Math.PI) {
+            dYaw -= Math.PI.toFloat() * 2f
+        }
+        while (dYaw < -Math.PI) {
+            dYaw += Math.PI.toFloat() * 2f
+        }
+
+        val yaw = te.bookRotationPrev + dYaw * partialTicks
+        rotate(-yaw * (180f / Math.PI.toFloat()), 0.0f, 1.0f, 0.0f)
+        rotate(80.0f, 0.0f, 0.0f, 1.0f)
+
+        var flipLeft = te.pageFlipPrev + (te.pageFlip - te.pageFlipPrev) * partialTicks + 0.25f
+        var flipRight = te.pageFlipPrev + (te.pageFlip - te.pageFlipPrev) * partialTicks + 0.75f
+        flipLeft = (flipLeft - MathHelper.truncateDoubleToInt(flipLeft.toDouble()).toFloat()) * 1.6f - 0.3f
+        flipRight = (flipRight - MathHelper.truncateDoubleToInt(flipRight.toDouble()).toFloat()) * 1.6f - 0.3f
+        if (flipLeft < 0.0f) {
+            flipLeft = 0.0f
+        }
+        if (flipRight < 0.0f) {
+            flipRight = 0.0f
+        }
+        if (flipLeft > 1.0f) {
+            flipLeft = 1.0f
+        }
+        if (flipRight > 1.0f) {
+            flipRight = 1.0f
+        }
+
+        val spread = te.bookSpreadPrev + (te.bookSpread - te.bookSpreadPrev) * partialTicks
+
+        this.bindTexture(BOOK_TEXTURE)
+        enableCull()
+        this.book.render(null, hover, flipLeft, flipRight, spread, 0.0f, 0.0625f)
+        // End of Vanilla part
+
+        popMatrix()
+
+        // We only draw the page content if its actually visible (i.e. book is not closed)
+        if (spread > 0) {
+            renderPages(te, x, y, z, lapis, yaw, hover, flipLeft, spread)
+        }
+
+        // Renders the items
+        color(1f, 1f, 1f, 1f)
+        translate(x + 0.5, y + 1.4, z + 0.5)
+
+        Minecraft.getMinecraft().textureManager.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE)
+        enableRescaleNormal()
+        RenderHelper.enableStandardItemLighting()
+
+        // Seed the animation randomizer with the TE's position, this will make the animations consistent
+        val rand = Random(te.pos.toLong())
+        val bobRand = rand.nextInt(100)
+        // The bobbing should stop momentarily when there is a result
+        val bobTicks = if (te.result == null) te.tickCount + partialTicks else te.bobStop
+        val bob = MathHelper.sin((bobTicks + bobRand) / 10.0f) * 0.1 + 0.1
+        // Only perform the upwards animation if there is a result
+        val t =
+            if (te.result == null)
+                0.0
+            else
+                Math.min(te.progress + partialTicks, 40f) / 40.0
+        // Only perform the upwards animation if there is a result
+        val inputAnim =
+            if (te.result != null)
+                bob + (0.3 - bob) * t
+            else
+                0.0
+        renderItem(te, Slot.OBJECT, 0.0, bob + inputAnim, 0.0)
+
+        // Get the offsets for each modifier's animation before they are used
+        // Otherwise there might be inconsistency for the positions
+        val lapisOffsets = listOf(rand.nextInt(720), rand.nextInt(720), rand.nextInt(720))
+        val rotationOffset = rand.nextInt(50)
+
+        // Render the modifiers in a circle around the object to enchant
+        for (i in 1..lapis) {
+            val offset = te.tickCount + rotationOffset + partialTicks
+            val bobAngle = Math.toRadians(offset * 4.0 + lapisOffsets[i - 1])
+            val angle = Math.toRadians((360.0 / modifiers!!.stackSize) * i + offset).toFloat()
+            val currentX = MathHelper.sin(angle) * 0.5
+            val currentZ = MathHelper.cos(angle) * 0.5
+            renderItem(te, Slot.MODIFIERS, currentX, Math.sin(bobAngle) * 0.1, currentZ)
+        }
+
+        RenderHelper.disableStandardItemLighting()
+        disableRescaleNormal()
+
+        popMatrix()
     }
 
     /**
